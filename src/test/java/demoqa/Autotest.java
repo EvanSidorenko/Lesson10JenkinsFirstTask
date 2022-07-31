bbc0cff62421a8f680908532e2464a9653ddd24f
Ivan <sidorenko.evan@yandex.ru>
2022-07-31 17:37:10 +0300

    added autotest class

---
+++ src/test/java/demoqa/Autotest.java
@@ -0,0 +1,74 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+@Tag("autotest")
+public class Autotest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1280x609";
+    }
+
+    @Test
+    void fillFormTest() {
+        String firstName = "Thomas";
+        String lastName = "Anderson";
+        String userEmail = "neo@matrix.com";
+        String userNumber = "9998887766";
+        String address = "Liberty st, room 101";
+
+        step("Open registration form", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('footer').remove()");
+            executeJavaScript("$('#fixedban').remove()");
+        });
+
+        step("Fill registration form", () -> {
+            $("#firstName").setValue(firstName);
+            $("#lastName").setValue(lastName);
+            $("#userEmail").setValue(userEmail);
+            $("#genterWrapper").$(byText("Male")).click();
+            $("#userNumber").setValue(userNumber);
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOptionByValue("0");
+            $(".react-datepicker__year-select").selectOptionByValue("1992");
+            $(".react-datepicker__day.react-datepicker__day--013").click();
+            $("#subjectsInput").setValue("English").pressEnter();
+            $("#hobbiesWrapper").$(byText("Reading")).click();
+            $("#uploadPicture").uploadFromClasspath("download.jpg");
+            $("#currentAddress").setValue(address);
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("Haryana")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Karnal")).click();
+            $("#submit").click();
+        });
+
+        step("Verify form data", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(
+                    text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(
+                    text(firstName + " " + lastName),
+                    text(userEmail),
+                    text("Male"),
+                    text("13 January,1992"),
+                    text("English"),
+                    text("Reading"),
+                    text("download.jpg"),
+                    text(address),
+                    text("Haryana Karnal"));
+        });
+    }
+}
