ded69c9b579d0aff9c34bceaee29c9a9df45757b
Ivan <sidorenko.evan@yandex.ru>
2022-07-31 16:03:33 +0300

    First commit

---
+++ src/test/java/simple/test/NegativeTest.java
@@ -0,0 +1,33 @@
+package simple.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTest {
+    @Test
+    void test00() {
+        assertFalse(false);
+    }
+
+    @Test
+    void test01() {
+        assertFalse(false);
+    }
+
+    @Test
+    void test02() {
+        assertFalse(false);
+    }
+
+    @Test
+    void test03() {
+        assertFalse(false);
+    }
+
+    @Test
+    void test04() {
+        assertTrue(false);
+    }
+}
